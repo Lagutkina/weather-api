5ca95a4c883c2b0fc5ee7f18629bf5b0a3f0946f
Anastasia Lagutkina <mummimora@gmail.com>
2021-01-27 12:32:43 +0100

    вывод дня на экран - упрощение

--- src/server.js
+++ src/server.js
@@ -2,41 +2,12 @@ const WEEK_DAY_OFFSET = 2
 
 export default () => {
   const weekDay = Math.floor(Math.random() * 7) - 1
-  const realWeekDayNumber = logRealWeekDay(weekDay)
+  const realWeekDayNumber = weekDay + WEEK_DAY_OFFSET
   const dayName = getName(realWeekDayNumber)
   console.log(dayName)
 }
 
-const logRealWeekDay = weekDay => {
-  const realWeekDay = summ(weekDay, WEEK_DAY_OFFSET)
-  // console.log('Real day = ' + realWeekDay)
-  return realWeekDay
-}
-
-const summ = (a, b) => {
-  return a + b
-}
-
 const getName = day => {
-  if (day === 1) {
-    return 'Mon'
-  }
-  if (day === 2) {
-    return 'Tue'
-  }
-  if (day === 3) {
-    return 'Wed'
-  }
-  if (day === 4) {
-    return 'Thu'
-  }
-  if (day === 5) {
-    return 'Fri'
-  }
-  if (day === 6) {
-    return 'Sat'
-  }
-  if (day === 7) {
-    return 'Sun'
-  }
+  const WEEK_DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  return WEEK_DAYS[day - 1]
 }
