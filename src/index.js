8c1184a4e2bbae31fa280efd0bf64f1407ae700a
Anastasia Lagutkina <mummimora@gmail.com>
2021-01-27 12:19:21 +0100

    вывод дня на экран

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Главный файл, он же сервисный (тут)
 import 'app-module-path/register'
 import server from './server'
 server()
--- src/server.js
+++ src/server.js
@@ -1,3 +1,43 @@
+const WEEK_DAY_OFFSET = 2
+
 export default () => {
   console.log('Hello World')
+  const weekDay = Math.floor(Math.random() * 7) - 1
+  const realWeekDayNumber = logRealWeekDay(weekDay)
+  const dayName = getName(realWeekDayNumber)
+  console.log(dayName)
+}
+
+const logRealWeekDay = weekDay => {
+  const realWeekDay = summ(weekDay, WEEK_DAY_OFFSET)
+  // console.log('Real day = ' + realWeekDay)
+  return realWeekDay
+}
+
+const summ = (a, b) => {
+  return a + b
+}
+
+const getName = day => {
+  if (day === 1) {
+    return 'Mon'
+  }
+  if (day === 2) {
+    return 'Tue'
+  }
+  if (day === 3) {
+    return 'Wed'
+  }
+  if (day === 4) {
+    return 'Thu'
+  }
+  if (day === 5) {
+    return 'Fri'
+  }
+  if (day === 6) {
+    return 'Sat'
+  }
+  if (day === 7) {
+    return 'Sun'
+  }
 }
